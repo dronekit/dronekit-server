776d289a2ca4d2ec279d1ee2189906dbbcfafb67
Kevin Hester <kevinh@geeksville.com>
2013-12-25 10:20:29 -0800

    don't screw up resource loading when adding srcs

--- project/build.scala
+++ project/build.scala
@@ -58,7 +58,8 @@ object NestorBuild extends Build {
       resolvers += "Maven snapshots" at "http://download.java.net/maven/2",
 
       // To include source for Takipi
-      unmanagedResourceDirectories in Compile <+= baseDirectory( _ / "src" ),
+      unmanagedResourceDirectories in Compile <+= baseDirectory( _ / "src" / "main" / "scala" ),
+      unmanagedResourceDirectories in Compile <+= baseDirectory( _ / "src" / "main" / "java" ),
 
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
