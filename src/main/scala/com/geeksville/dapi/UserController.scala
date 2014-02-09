eec9dfa11c7505ae06b1e414f982e43287df5a7c
Kevin Hester <kevinh@geeksville.com>
2014-02-09 12:20:03 -0800

    add crud operations

--- src/main/scala/com/geeksville/dapi/UserController.scala
+++ src/main/scala/com/geeksville/dapi/UserController.scala
@@ -12,52 +12,77 @@ class UserController(implicit val swagger: Swagger) extends ScalatraServlet with
   protected implicit val jsonFormats: Formats = DefaultFormats
 
   override protected val applicationName = Some("user")
-  protected val applicationDescription = "The flowershop API. It exposes operations for browsing and searching lists of flowers, and retrieving single flowers."
+  protected val applicationDescription = "The user API. It exposes operations for browsing and searching lists of users, and retrieving single user."
 
   // Before every action runs, set the content type to be in JSON format.
   before() {
     contentType = formats("json")
   }
 
-  val getFlowers =
-    (apiOperation[List[Flower]]("getFlowers")
-      summary "Show all flowers"
-      notes "Shows all the flowers in the flower shop. You can search it too."
+  private val getUsers =
+    (apiOperation[List[User]]("getUsers")
+      summary "Show all users"
+      notes "Shows all the users. You can search it too."
       parameter queryParam[Option[String]]("name").description("A name to search for"))
 
   /*
-   * Retrieve a list of flowers
+   * Retrieve a list of users
    */
-  get("/", operation(getFlowers)) {
+  get("/", operation(getUsers)) {
     params.get("name") match {
-      case Some(name) => FlowerData.all filter (_.name.toLowerCase contains name.toLowerCase())
-      case None => FlowerData.all
+      case Some(name) => UserData.all filter (_.name.toLowerCase contains name.toLowerCase())
+      case None => UserData.all
     }
   }
+
+  private val findById =
+    (apiOperation[User]("findById")
+      summary "Find by id"
+      parameters (
+        pathParam[String]("id").description("Id of user that needs to be fetched")))
 
   /**
    * Find a flower using its slug.
    */
-  get("/:slug") {
-    FlowerData.all find (_.slug == params("slug")) match {
+  get("/:id", operation(findById)) {
+    UserData.all find (_.id == params("id")) match {
       case Some(b) => b
       case None => halt(404)
     }
   }
 
+  private val createById =
+    (apiOperation[String]("createById")
+      summary "Create by id"
+      parameters (
+        pathParam[String]("id").description("Id of user that needs to be created")))
+
+  post("/:id", operation(createById)) {
+    halt(404)
+  }
+
+  private val deleteById =
+    (apiOperation[String]("deleteById")
+      summary "Delete by id"
+      parameters (
+        pathParam[String]("id").description("Id of user that needs to be deleted")))
+
+  delete("/:id", operation(deleteById)) {
+    halt(404)
+  }
 }
 
 // A Flower object to use as a faked-out data model
-case class Flower(slug: String, name: String)
+case class User(id: String, name: String)
 
 // An amazing datastore!
-object FlowerData {
+object UserData {
 
   /**
    * Some fake flowers data so we can simulate retrievals.
    */
   var all = List(
-    Flower("yellow-tulip", "Yellow Tulip"),
-    Flower("red-rose", "Red Rose"),
-    Flower("black-rose", "Black Rose"))
+    User("uid4", "Bob Someone"),
+    User("uid5", "Bob Someone2"),
+    User("uid6", "Bob Someone3"))
 }
