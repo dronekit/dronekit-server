5bb11a35dccc81dc9701947555f8308e986b5b06
Kevin Hester <kevinh@geeksville.com>
2014-03-23 15:56:43 -1000

    Move a few more vars to case vars, so auto JSON works better

--- src/main/scala/com/geeksville/dapi/model/Mission.scala
+++ src/main/scala/com/geeksville/dapi/model/Mission.scala
@@ -65,27 +65,22 @@ object MissionSummary extends DapiRecordCompanion[MissionSummary] {
 /**
  * A mission recorded from a vehicle
  */
-case class Mission() extends DapiRecord {
+case class Mission(
+  // As specified by the user
+  var notes: Option[String] = None,
+  // Is the client currently uploading data to this mission
+  var isLive: Boolean = false,
+  var viewPrivacy: Int = AccessCode.DEFAULT_VALUE,
+
+  var controlPrivacy: Int = AccessCode.DEFAULT_VALUE) extends DapiRecord {
   /**
    * What vehicle made me?
    */
   lazy val vehicle = belongsTo[Vehicle]
   val vehicleId: Option[Long] = None
 
-  // As specified by the user
-  var notes: Option[String] = None
-
-  /**
-   * Is the client currently uploading data to this mission
-   */
-  var isLive: Boolean = false
-
   @Transient
   var keep: Boolean = true
-
-  var viewPrivacy: Int = AccessCode.DEFAULT_VALUE
-
-  var controlPrivacy: Int = AccessCode.DEFAULT_VALUE
 
   /**
    * If the tlog is stored to s3 this is the ID
--- src/main/scala/com/geeksville/dapi/model/Vehicle.scala
+++ src/main/scala/com/geeksville/dapi/model/Vehicle.scala
@@ -12,7 +12,25 @@ import com.github.aselab.activerecord.dsl._
  *
  * @param uuid is selected by the client on first connection
  */
-case class Vehicle(@Required @Unique uuid: UUID = UUID.randomUUID()) extends DapiRecord {
+case class Vehicle(
+  @Required @Unique uuid: UUID = UUID.randomUUID(),
+  // A user specified name for this vehicle (i.e. my bixler)
+  var name: String = "",
+
+  // Vehicle manufacturer if known, preferably from the master vehicle-mfg.txt definitions file.  
+  // To add new definitions to the file, please submit a github pull-request.
+  var manufacturer: Option[String] = None,
+
+  // Vehicle type if known, preferably from the master vehicle-types.txt definitions file.  
+  // To add new definitions to the file, please submit a github pull-request.
+  var vehicleType: Option[String] = None,
+
+  // Autopilot type if known, preferably from the master autopilot-types.txt definitions file.
+  // To add new definitions to the file, please submit a github pull-request.
+  var autopilotType: Option[String] = None,
+
+  // Autopilot software version #
+  var softwareVersion: Option[String] = None) extends DapiRecord {
   /**
    * Who owns me?
    */
@@ -20,29 +38,9 @@ case class Vehicle(@Required @Unique uuid: UUID = UUID.randomUUID()) extends Dap
   val userId: Option[Long] = None
 
   /**
-   * A user specified name for this vehicle (i.e. my bixler)
-   */
-  var name: String = ""
-
-  /**
    * All the missions this vehicle has made
    */
   lazy val missions = hasMany[Mission]
-
-  // Vehicle manufacturer if known, preferably from the master vehicle-mfg.txt definitions file.  
-  // To add new definitions to the file, please submit a github pull-request.
-  var manufacturer: Option[String] = None
-
-  // Vehicle type if known, preferably from the master vehicle-types.txt definitions file.  
-  // To add new definitions to the file, please submit a github pull-request.
-  var vehicleType: Option[String] = None
-
-  // Autopilot type if known, preferably from the master autopilot-types.txt definitions file.
-  // To add new definitions to the file, please submit a github pull-request.
-  var autopilotType: Option[String] = None
-
-  // Autopilot software version #
-  var softwareVersion: Option[String] = None
 }
 
 object Vehicle extends DapiRecordCompanion[Vehicle] {
