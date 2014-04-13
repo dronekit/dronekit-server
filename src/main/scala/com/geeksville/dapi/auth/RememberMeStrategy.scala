a9812495bf586553214c882eec2fe56abaee0661
Kevin Hester <kevinh@geeksville.com>
2014-04-13 11:46:40 -1000

    make secure md5 cookies for logins

--- src/main/scala/com/geeksville/dapi/auth/RememberMeStrategy.scala
+++ src/main/scala/com/geeksville/dapi/auth/RememberMeStrategy.scala
@@ -6,13 +6,15 @@ import javax.servlet.http.{ HttpServletResponse, HttpServletRequest }
 import org.slf4j.LoggerFactory
 import com.geeksville.dapi.model.User
 import grizzled.slf4j.Logging
+import org.apache.commons.codec.binary.Base64
+import java.security.MessageDigest
 
 class RememberMeStrategy(protected val app: ScalatraBase)(implicit request: HttpServletRequest, response: HttpServletResponse)
   extends ScentryStrategy[User] with Logging {
 
   override def name: String = "RememberMe"
 
-  val COOKIE_KEY = "rememberMe"
+  private val COOKIE_KEY = "rememberMe"
   private val oneWeek = 7 * 24 * 3600
 
   def shouldUseCookies = {
@@ -40,8 +42,102 @@ class RememberMeStrategy(protected val app: ScalatraBase)(implicit request: Http
    * Determine whether the strategy should be run for the current request.
    */
   override def isValid(implicit request: HttpServletRequest): Boolean = {
-    logger.info("RememberMeStrategy: determining isValid: " + (tokenVal != "").toString())
-    tokenVal != ""
+    // Note: I was using the less expensive getUserIdFromToken here - but that isn't good enough.  If the user has changed
+    // their password we want remember me to say the token should be ignored
+    val probablyGood = getUserFromToken(tokenVal).isDefined
+    logger.info(s"RememberMeStrategy: determining isValid: $probablyGood")
+    probablyGood
+  }
+
+  /**
+   * Change this token to invalidate all cookies.  FIXME - much better to generate randomly once and store
+   * in nestor.conf (not in source control).
+   */
+  private val tokenKey = 432879274L
+
+  private val encoder = new Base64
+
+  private def md5hex(s: String) =
+    {
+      val md5digest = MessageDigest.getInstance("MD5")
+      md5digest.digest(s.getBytes).map { "%02x".format(_) }.mkString
+    }
+
+  /**
+   * This function is used both at token generation and later to check the generated token
+   */
+  private def makeMd5String(u: User, expireMsec: Long) =
+    md5hex(u.login + ":" + expireMsec + ":" + u.hashedPassword + ":" + tokenKey)
+
+  /**
+   * Per
+   * http://docs.spring.io/spring-security/site/docs/3.0.x/reference/remember-me.html
+   *     base64(username + ":" + expirationTime + ":" +
+   * md5Hex(username + ":" + expirationTime + ":" password + ":" + key))
+   *
+   * username:          As identifiable to the UserDetailsService
+   * password:          That matches the one in the retrieved UserDetails
+   * expirationTime:    The date and time when the remember-me token expires,
+   * expressed in milliseconds
+   * key:               A private key to prevent modification of the remember-me token
+   */
+  private def makeToken(u: User) = {
+    val expireMsec = System.currentTimeMillis + oneWeek * 1000L
+
+    val md5 = makeMd5String(u, expireMsec)
+    val s = u.login + ":" + expireMsec + ":" + md5
+
+    logger.warn(s"Generated token $s")
+    encoder.encodeToString(s.getBytes)
+  }
+
+  private val TokenRegex = "(.*):(.*):(.*)".r
+  private val MD5Regex = "(.*):(.*):(.*):(.*)".r
+
+  /**
+   *  This does most of the 'cheap' validation on the token, returning the userId if the md5 sum mostly checked out.
+   *  @return (userid, expire, md5)
+   */
+  private def getUserIdFromToken(t: String): Option[(String, Long, String)] = {
+    val sbytes = encoder.decode(t)
+    val s = sbytes.map(_.toChar).mkString
+    logger.debug(s"Checking base64 $t -> token $s")
+    s match {
+      case TokenRegex(login, expire, md5) =>
+
+        val e = expire.toLong
+        if (e < System.currentTimeMillis) {
+          logger.warn(s"Token too old, deny.")
+          None
+        } else
+          Some(login, e, md5)
+      case _ =>
+        logger.error(s"HACK ATTEMPT: malformed token: $t")
+        None
+    }
+  }
+
+  /**
+   * Complete validation of a token (including checking user password).  Returns user object on success
+   */
+  private def getUserFromToken(t: String): Option[User] = {
+    getUserIdFromToken(t).flatMap {
+      case (login, expire, md5) =>
+        User.find(login).flatMap { u =>
+
+          val expectedMd5 = makeMd5String(u, expire)
+          if (expectedMd5 != md5) {
+            logger.error(s"Bad token for $u - deny! (hack attempt or user changed psw)")
+            None
+          } else {
+            logger.warn(s"$login has good token")
+            Some(u)
+          }
+        }.orElse {
+          logger.warn(s"User $login not found")
+          None
+        }
+    }
   }
 
   /**
@@ -52,8 +148,7 @@ class RememberMeStrategy(protected val app: ScalatraBase)(implicit request: Http
    */
   def authenticate()(implicit request: HttpServletRequest, response: HttpServletResponse) = {
     logger.info("RememberMeStrategy: attempting authentication")
-    if (tokenVal == "foobar") Some(User("foo"))
-    else None
+    getUserFromToken(tokenVal)
   }
 
   /**
@@ -72,10 +167,9 @@ class RememberMeStrategy(protected val app: ScalatraBase)(implicit request: Http
    * more than 1 cookie.
    */
   override def afterAuthenticate(winningStrategy: String, user: User)(implicit request: HttpServletRequest, response: HttpServletResponse) = {
-    logger.info("rememberMe: afterAuth fired")
     if (winningStrategy == "RememberMe" || shouldUseCookies) {
-
-      val token = "foobar"
+      logger.info("rememberMe: setting cookie")
+      val token = makeToken(user)
       app.cookies.set(COOKIE_KEY, token)(CookieOptions(maxAge = oneWeek, path = "/"))
     }
   }
