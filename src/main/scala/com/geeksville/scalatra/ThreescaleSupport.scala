62f0ddf94651bd521b3674b52d1be00b7007166c
Kevin Hester <kevinh@geeksville.com>
2014-04-16 16:55:19 -1000

    Prefer api_key in a RFC2617 header

--- src/main/scala/com/geeksville/scalatra/ThreescaleSupport.scala
+++ src/main/scala/com/geeksville/scalatra/ThreescaleSupport.scala
@@ -11,11 +11,18 @@ import threescale.v3.api.AuthorizeResponse
 import com.geeksville.akka.MockAkka
 import akka.actor.Props
 import com.geeksville.threescale.ThreeActor
+import scala.collection.JavaConverters._
+
+object ThreescaleSupport {
+  private val HeaderRegex = "DroneApi apikey=\"(.*)\"".r
+}
 
 /**
  * A mixin that adds API validation through threescale
  */
 trait ThreescaleSupport extends ScalatraBase with ControllerExtras {
+  import ThreescaleSupport._
+
   // FIXME - we should not be making a separate threescale actor for each endpoint
   private lazy val threeActor: ActorRef = MockAkka.system.actorOf(Props(new ThreeActor(MockAkka.config.getString("threescale.apiKey"))))
   private lazy val service = MockAkka.config.getString("threescale.serviceId")
@@ -26,12 +33,30 @@ trait ThreescaleSupport extends ScalatraBase with ControllerExtras {
   }
 
   /**
+   * Look for API key in an authorization header, or if not there, then in the query string.
+   */
+  private def apiKey = {
+    val headerkeys = request.getHeaders("Authorization").asScala.flatMap { s =>
+      s match {
+        case HeaderRegex(key) => Some(key)
+        case _ => None
+      }
+    }.toSeq
+
+    if (headerkeys.isEmpty)
+      params.getOrElse("api_key", haltUnauthorized("api key is required. See http://nestor.3dr.com/develop"))
+    else if (headerkeys.size > 1)
+      haltBadRequest("Too many API keys")
+    else
+      headerkeys.head
+  }
+
+  /**
    * Check for authorization to use serviceId X.  will haltUnauthorized if quota exceeded
    */
   def requireServiceAuth(metrics: Map[String, String]) {
-    // FIXME include a better URL
-    val userkey = params.getOrElse("api_key", haltUnauthorized("api key is required. See http://nestor.3dr.com/develop"))
-    val req = AuthRequest(userkey, service, metrics)
+    // FIXME include a better URL for developer site
+    val req = AuthRequest(apiKey, service, metrics)
 
     implicit val timeout = Timeout(5 seconds)
     val future = threeActor ? req
