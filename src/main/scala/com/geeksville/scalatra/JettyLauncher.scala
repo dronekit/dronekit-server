25850cd68938870890510e02f803dd1fc7630ea4
Kevin Hester <kevinh@geeksville.com>
2014-05-24 14:59:08 -0700

    turn off removed code

--- src/main/scala/com/geeksville/scalatra/JettyLauncher.scala
+++ src/main/scala/com/geeksville/scalatra/JettyLauncher.scala
@@ -1,14 +1,16 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2013 Kevin Hester
- * 
+ *
  * See LICENSE.txt for license details.
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package com.geeksville.scalatra
 
 import org.eclipse.jetty.server.Server
@@ -24,8 +26,8 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
     val context = new WebAppContext()
     context setContextPath "/"
     context.setResourceBase("src/main/webapp")
-    context.addServlet(classOf[MainServlet], "/*")
-    context.addServlet(classOf[DeviceServlet], "/api/*")
+    //context.addServlet(classOf[MainServlet], "/*")
+    //context.addServlet(classOf[DeviceServlet], "/api/*")
     // context.addServlet(classOf[DefaultServlet], "/")
 
     server.setHandler(context)
