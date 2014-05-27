144dcf7b12ede37126a716b1daa41be3f45aa7bc
Kevin Hester <kevinh@geeksville.com>
2014-05-27 13:06:11 -0700

    better keys

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -13,10 +13,10 @@ echo
 
 # we send up src as a super skanky hack because our assembly still accidentally references
 # src/main/webapp/WEB-INF
-rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa-3dr" drone-mysql.sh target/scala-2.10/apihub-assembly-*.jar ubuntu@$EC2_HOSTNAME:
-rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa-3dr" src/main/webapp ubuntu@$EC2_HOSTNAME:src/main
+rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa_dronehub" drone-mysql.sh target/scala-2.10/apihub-assembly-*.jar ubuntu@$EC2_HOSTNAME:
+rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa_dronehub" src/main/webapp ubuntu@$EC2_HOSTNAME:src/main
 
-rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa-3dr" S98nestor-startup ubuntu@$EC2_HOSTNAME:/tmp
+rsync -avz -e "ssh -l ubuntu -i /home/kevinh/.ssh/id_dsa_dronehub" S98nestor-startup ubuntu@$EC2_HOSTNAME:/tmp
 ./ssh-ec2 sudo mv /tmp/S98nestor-startup /etc/rc2.d
 
 TAGNAME=deploy-`date +%F-%H%M%S`
