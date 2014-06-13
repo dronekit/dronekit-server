7116a645a81a9f0716d887f6a7e301469838a8d6
Kevin Hester <kevinh@geeksville.com>
2014-06-13 10:59:38 -0700

    dflash upload approximately works

--- src/main/scala/com/geeksville/dapi/DataflashPlaybackModel.scala
+++ src/main/scala/com/geeksville/dapi/DataflashPlaybackModel.scala
@@ -33,31 +33,55 @@ class DataflashPlaybackModel extends PlaybackModel {
 
     val reader = new DFReader
     var nowMsec = 0L
-    def nowUsec = nowMsec * 1000
+    var gpsOffsetUsec = 0L
+    def nowUsec = gpsOffsetUsec + nowMsec * 1000
 
     reader.parseText(Source.fromRawBytes(bytes)).foreach { m =>
-      debug(s"Considering $m")
-      m.timeMSopt.foreach(nowMsec = _)
+      def dumpMessage() = debug(s"Considering $m")
+
+      def updateTime() {
+        m.timeMSopt.foreach(nowMsec = _)
+        currentTime = Some(nowUsec)
+      }
 
       m.typ match {
         case GPS =>
+          //dumpMessage()
+
+          // Cancel out the last known msec offset, using GPS time as the new zero
+          gpsOffsetUsec = m.gpsTimeUsec - (nowMsec * 1000)
+          if (!startTime.isDefined)
+            startTime = Some(nowUsec)
+
           for {
             lat <- m.latOpt
             lon <- m.lngOpt
           } yield {
             val loc = Location(lat, lon, m.altOpt)
             val tm = TimestampedLocation(nowUsec, loc)
-            debug(s"Adding location $tm")
+            //debug(s"Adding location $tm")
             positions.append(tm)
+            endPosition = Some(loc)
           }
 
+        case IMU =>
+          updateTime()
+
+        case ATT =>
+          updateTime()
+
         case MODE =>
+          dumpMessage()
+
         case PARM =>
+          dumpMessage()
+
           val msg = new msg_param_value(0, 0) // FIXME - params shouldn't assume mavlink msgs, but for now...
           val name = m.name
           msg.setParam_id(name)
           msg.param_value = m.value.toFloat
           params(name) = new ROParamValue(msg)
+
         case _ =>
         // Ignore
       }
