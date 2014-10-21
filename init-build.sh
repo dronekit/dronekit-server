570531277d53941f4c628e57d116a3fec012c792
Kevin Hester <kevinh@geeksville.com>
2014-10-21 11:08:38 +0900

    fixes to make vagrant work better.  changes: * remove sbtopts for debugger support - not always needed and slows things * remove dependencies dir during provision - so we are guaranteed a clean build * use anon urls to fetch my various public git repos * add -y to apt-get so it doesn't freak out by being unable to ask user * run init-build as the vagrant user rather than root

--- init-build.sh
+++ init-build.sh
@@ -9,6 +9,8 @@ git submodule update --recursive --init
 
 cp nestor.conf.template ~/nestor.conf
 
+echo rebuilding dependencies
+rm -rf dependencies
 mkdir dependencies
 cd dependencies
 
@@ -19,12 +21,12 @@ cd dependencies
 #ln -s ../dependencies/$SCALA/bin/* .
 #popd
 
-git clone git@github.com:geeksville/sbt-scalabuff.git
+git clone https://github.com/geeksville/sbt-scalabuff.git
 cd sbt-scalabuff/
 sbt publishLocal
 cd ..
 
-git clone -b fixes_for_dronehub git@github.com:geeksville/json4s.git
+git clone -b fixes_for_dronehub https://github.com/geeksville/json4s.git
 cd json4s
 sbt publishLocal
 cd ..
@@ -36,12 +38,12 @@ cd akka
 sbt -Dakka.scaladoc.diagrams=false publishLocal
 cd ..
 
-git clone -b 2.3.x_2.10 git@github.com:geeksville/scalatra.git
+git clone -b 2.3.x_2.10 https://github.com/geeksville/scalatra.git
 cd scalatra
 sbt publishLocal
 cd ..
 
-git clone git@github.com:geeksville/scala-activerecord.git
+git clone https://github.com/geeksville/scala-activerecord.git
 cd scala-activerecord
 sbt "project core" publishLocal "project generator" publishLocal "project scalatra" publishLocal "project scalatraSbt" publishLocal
 cd ..
--- initvagrant.sh
+++ initvagrant.sh
@@ -1,15 +1,20 @@
-echo "PROVISIONING DroneAPI-Private vagrant instance.  You'll need to manually edit ~/nestor.conf"
-sudo apt-get update
-sudo apt-get install git python-pip openjdk-6-jre-headless openjdk-7-jdk python-dev build-essential python-numpy
+echo "PROVISIONING DroneAPI-Private vagrant instance."
+sudo apt-get -y update
+sudo apt-get -y install git python-pip openjdk-6-jre-headless openjdk-7-jdk python-dev build-essential python-numpy
 pushd /tmp
 wget http://dl.bintray.com/sbt/debian/sbt-0.13.5.deb
 sudo dpkg -i sbt-0.13.5.deb
 popd
-cp -a ~/host-ssh/id* ~/.ssh
-
 echo "INITING JAVA/SBT build dependancies - this will take a while..."
+# Note - this script is run as root, so change to the vagrant user for remaining steps
+sudo --login --set-home -u vagrant bash << EOF
+cp -a /home/vagrant/host-ssh/id* /home/vagrant/.ssh
+
+echo "Initing build dependencies"
 cd /vagrant
-./init-build
+./init-build.sh
+
 echo "Doing an initial build"
 sbt compile
-
+EOF
+echo "Done PROVISIONING DroneAPI-Private You'll need to manually edit /home/vagrant/nestor.conf"
