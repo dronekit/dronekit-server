c6f343732c9cc80bd623a35958e02efdfadfd5ff
Kevin Hester <kevinh@geeksville.com>
2014-12-31 09:13:11 -0800

    turn oauth unit test back on

--- src/test/scala/com/geeksville/dapi/OAuthTests.scala
+++ src/test/scala/com/geeksville/dapi/OAuthTests.scala
@@ -30,7 +30,7 @@ import org.json4s.JsonDSL._
 /**
  * These tests can be disabled by adding an argument to the constructor.
  */
-class OAuthTests(disabled: Boolean) extends ServerDependentSuite {
+class OAuthTests extends ServerDependentSuite {
 
   // We want cookies for this test
   session {
